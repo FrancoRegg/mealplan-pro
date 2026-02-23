78c7d911f056f5db4dc31489f56d20e73f8f6a1b
FrancoRegg <francoreggiardo97@gmail.com>
2026-02-23 16:39:19 +0100

    User registration view + data validation

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class UserRegisterView(APIView):
+    permission_classes = [AllowAny] #Indica que este endpoint es publico, no necesita permisos de ningun tipo
+    def post(self, request):
+        
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
