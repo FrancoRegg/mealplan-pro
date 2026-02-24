ac5a25957cf33eedd8aa92c36f3901355e860217
FrancoRegg <francoreggiardo97@gmail.com>
2026-02-24 16:11:57 +0100

    Installing token_backlist

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     #Librerias externas
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
 
     #Aplicaciones del proyecto
