dca78abd20929525ffa8d5a1412a282cdbfeb788
FrancoRegg <francoreggiardo97@gmail.com>
2026-02-24 16:12:38 +0100

    Implementation of login and logout endpoints

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import UserRegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='register')
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login' ),
+    path('login/refresh/', TokenRefreshView.as_view(), name='login-refresh' ),
+    path('logout/', TokenBlacklistView.as_view(), name='logout' )
 ]
