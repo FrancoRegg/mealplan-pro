b1c77605310acc5166aa53981d0629110025c1e9
FrancoRegg <francoreggiardo97@gmail.com>
2026-02-27 12:31:33 +0100

    Full view for obtaining, creating, and updating user profile data

--- backend/apps/profiles/views.py
+++ backend/apps/profiles/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+class ProfileView(APIView):
+    def get(self, request):
+        try:
+            serializer = ProfileSerializer(request.user.profile)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            return Response({'error': 'Perfil no encontrado. Complete su perfil.'}, status=status.HTTP_404_NOT_FOUND)
+    
+    def put(self, request):
+        try:
+            serializer = ProfileSerializer(request.user.profile, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Profile.DoesNotExist:
+            serializer = ProfileSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(user=request.user)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
