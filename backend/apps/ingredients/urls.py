98b32a7a21f1be297fb7fd7d1ce5a89519fd4bdb
FrancoRegg <francoreggiardo97@gmail.com>
2026-03-03 20:06:26 +0100

    Connection with the operation of the PUT endpoint

--- backend/apps/ingredients/urls.py
+++ backend/apps/ingredients/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IngredientView
+from .views import *
 
 urlpatterns = [
-    path('ingredients/', IngredientView.as_view(), name="ingredient")
+    path('ingredients/', IngredientView.as_view(), name="ingredient"),
+    path('ingredients/availability/', IngredientAvailabilityView.as_view(), name="ingredient_available")
 ]
