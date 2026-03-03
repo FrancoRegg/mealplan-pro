1e7abd1fdf2571fc81dccb62fb1ae72028d13f9e
FrancoRegg <francoreggiardo97@gmail.com>
2026-03-03 17:43:41 +0100

    Get ingredient list

--- backend/apps/ingredients/views.py
+++ backend/apps/ingredients/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
+from apps.profiles.models import Profile
+from .serializers import IngredientSerializer
 
-# Create your views here.
+class IngredientView(APIView):
+    def get(self, request):
+        try:
+            serializer = IngredientSerializer(Ingredient.objects.all(), many=True, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            return Response({'error': 'Perfil no encontrado. Complete su perfil.'}, status=status.HTTP_404_NOT_FOUND)
