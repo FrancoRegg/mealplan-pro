e307631671990b75510c915f5cdc71ef23e968af
FrancoRegg <francoreggiardo97@gmail.com>
2026-03-14 18:16:52 +0100

    View to generate menu for a profile, use the ingredients that the user has available

--- backend/apps/menus/views.py
+++ backend/apps/menus/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
+from .serializers import *
+from apps.ingredients.models import Ingredient
+from apps.profiles.models import Profile
+from .services import generate_menu
 
-# Create your views here.
+class GenerateMenuView(APIView):
+    def post(self, request):
+        try:
+            profile = request.user.profile
+            available_ingredients = Ingredient.objects.filter(
+                profileingredient__profile=profile, 
+                profileingredient__is_available=True
+            )
+            # Verificamos que exista al menos un ingrediente para cada categoria 
+            proteins = [i for i in available_ingredients if i.category == 'protein']
+            carbohydrates = [i for i in available_ingredients if i.category == 'carb']
+            vegetables = [i for i in available_ingredients if i.category == 'vegetable']
+            if not proteins or not carbohydrates or not vegetables:
+                return Response({'error': 'Debe tener al menos una proteína, un carbohidrato y una verdura disponibles.'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # Despues de validar, llamamos a generate_menu() para obtener un menu completo
+            menu = generate_menu(profile, available_ingredients)
+            serializer_menu = MenuSerializer(menu)
+            return Response(serializer_menu.data, status=status.HTTP_201_CREATED)
+        except Profile.DoesNotExist:
+            return Response({'error': 'Perfil no encontrado. Complete su perfil.'}, status=status.HTTP_404_NOT_FOUND)
