02e7f564a867cb89cbdc5bf3c40c87d5b4753694
FrancoRegg <francoreggiardo97@gmail.com>
2026-03-15 12:23:13 +0100

    Path to obtain active menu

--- backend/apps/menus/urls.py
+++ backend/apps/menus/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('generate/', GenerateMenuView.as_view(), name="generate_menu")
+    path('generate/', GenerateMenuView.as_view(), name="generate_menu"),
+    path('active/', ActiveMenuView.as_view(), name="active_menu")
 ]
