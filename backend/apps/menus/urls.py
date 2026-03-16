5a5aa91c6029e435d03a57100bd8994d301ba661
FrancoRegg <francoreggiardo97@gmail.com>
2026-03-16 11:15:20 +0100

    Get a menu by its ID

--- backend/apps/menus/urls.py
+++ backend/apps/menus/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('generate/', GenerateMenuView.as_view(), name="generate_menu"),
-    path('active/', ActiveMenuView.as_view(), name="active_menu")
+    path('active/', ActiveMenuView.as_view(), name="active_menu"),
+    path('<int:menu_id>/', MenuByIdView.as_view(), name="menu_id")
 ]
