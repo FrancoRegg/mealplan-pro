76608ac179e11828842b7334422bb972c39d2a38
FrancoRegg <francoreggiardo97@gmail.com>
2026-03-18 13:24:21 +0100

    Menu regeneration

--- backend/apps/menus/urls.py
+++ backend/apps/menus/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('generate/', GenerateMenuView.as_view(), name="generate_menu"),
     path('active/', ActiveMenuView.as_view(), name="active_menu"),
-    path('<int:menu_id>/', MenuByIdView.as_view(), name="menu_id")
+    path('<int:menu_id>/', MenuByIdView.as_view(), name="menu_id"),
+    path('<int:menu_id>/days/<int:day_number>/regenerate', RegenerateDayView.as_view(), name="regenerate_menu")
 ]
