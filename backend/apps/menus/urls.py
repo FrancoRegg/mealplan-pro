95fbe2b2ed1b5fec76437dd2f1f044e984e85484
FrancoRegg <francoreggiardo97@gmail.com>
2026-03-19 16:56:15 +0100

    Lock/unlock for one day

--- backend/apps/menus/urls.py
+++ backend/apps/menus/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('generate/', GenerateMenuView.as_view(), name="generate_menu"),
     path('active/', ActiveMenuView.as_view(), name="active_menu"),
     path('<int:menu_id>/', MenuByIdView.as_view(), name="menu_id"),
-    path('<int:menu_id>/days/<int:day_number>/regenerate', RegenerateDayView.as_view(), name="regenerate_menu")
+    path('<int:menu_id>/days/<int:day_number>/regenerate/', RegenerateDayView.as_view(), name="regenerate_menu"),
+    path('<int:menu_id>/days/<int:day_number>/lock/', LockDayView.as_view(), name="lock_day")
 ]
