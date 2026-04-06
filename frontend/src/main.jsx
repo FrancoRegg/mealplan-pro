c7d8c1776cb8b158b8e73757d1d39fb2150953d0
FrancoRegg <francoreggiardo97@gmail.com>
2026-04-06 12:44:25 +0200

    App is wrapped with provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { ProfileProvider } from './context/ProfileContext.jsx'
+import { MenuProvider } from './context/MenuContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
@@ -9,7 +11,11 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ProfileProvider>
+          <MenuProvider>
+            <App />
+          </MenuProvider>
+        </ProfileProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
